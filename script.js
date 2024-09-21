6e4d249e8a81cde49ccc2af0be915223b073744a
Lorens Pierre Louis <lorens.pierlouis.nutri@gmail.com>
2024-09-21 00:06:16 -0300

    Está puesto la validación del contacto

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-//La validación del formulário con javascript es considerada un desafío extra, no es obligatório realizar esta validación para realizar su entrega
+/*Validación*/
+
+document.getElementById('contacto').addEventListener('submit', function(event) {
+    event.preventDefault(); // Evita el envío del formulario por defecto
+  
+    const nombre = document.getElementById('input1').value;
+    const email = document.getElementById('input2').value;
+    
+   
+  
+    // Validación simple
+    if (nombre === '' || email === '') {
+      alert("Por favor, completa todos los campos");
+      alert.style.color = 'red';
+    } else {
+      alert("Formulario enviado con éxito");
+      alert.style.color = 'green';
+    }
+  });
+  
